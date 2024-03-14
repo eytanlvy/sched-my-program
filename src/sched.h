1176dab2d6d9ac5f3d8a5d0c264ea88676b8ccda
Eytan Levy <eytanlevy@proton.me>
2024-03-14 14:18:54 +0100

    init folders

---
+++ src/sched.h
@@ -0,0 +1,12 @@
+struct scheduler;
+
+typedef void (*taskfunc)(void*, struct scheduler *);
+
+static inline int
+sched_default_threads()
+{
+    return sysconf(_SC_NPROCESSORS_ONLN);
+}
+
+int sched_init(int nthreads, int qlen, taskfunc f, void *closure);
+int sched_spawn(taskfunc f, void *closure, struct scheduler *s);
