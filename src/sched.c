ce662b83a0a12f423fe867c3f8661208fe0f0183
yael2718 <159441598+yael2718@users.noreply.github.com>
2024-03-27 10:13:22 +0100

    Update sched.c

--- src/sched.c
+++ src/sched.c
@@ -2,22 +2,34 @@
 #include<unistd.h>
 #include <pthread.h>
 
-struct scheduler{
+typedef struct task{
+    taskfunc function;
+    void* arg;
+} task;
+
+typedef struct scheduler{
     int nthreads;          // Nombre de threads
     int qlen;              // Nombre maximum de tâches simultanées
     pthread_t *threads;    // Tableau des threads
     int stack_size;        // Taille actuelle de la pile
-    taskfunc *stack;       // Pile de tâches
-};
+    task *stack;           // Pile de tâches
+} scheduler;
 
-
-int sched_init(int nthreads, int qlen, taskfunc f, void *closure){ //créé un ordonnanceur.
-    if(nthreads <= 0){
+int sched_init(int nthreads, int qlen, taskfunc f, void *closure){//créé un ordonnanceur.
+    if(nthreads < 0){
+        write(2,"[!] Error : wrong parameter in sched_init, nthreads must be >= 0\n",65);
+        return -1;
+    }
+    if(qlen <= 0){
+        write(2,"[!] Error : wrong parameter in sched_init, qlen must be > 0\n",60);
+        return -1;
+    }
+    if(!nthreads){
         nthreads = sched_default_threads(); 
     }
     return 1;
 }
 
 int sched_spawn(taskfunc f, void *closure, struct scheduler *s){
-    
+    return 1;
 }
